7ca18efc461893be6684a7a7d883eae2c886b7e0
hyundole2 <56394805+hyundole2@users.noreply.github.com>
2020-01-02 20:05:12 +0900

    January 2 #3.9

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "b7c4391e16128b5b696643d5b838c88c";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Can't access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    //getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
